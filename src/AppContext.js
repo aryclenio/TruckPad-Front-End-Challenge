6b2d0b1106282df6a25f2d611cd1b34f795bd031
aryclenio <aryclenio2010@gmail.com>
2020-06-30 16:58:49 -0300

    :sparkles: Finishing state requests from server

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,27 +1,21 @@
 import React, { useState, useEffect, createContext } from 'react';
+import api from './services/api'
 
 export const AppContext = createContext();
 
 
 export const Provider = (props) => {
-  const [drivers, setDrivers] = useState([
-    {
-      "name": "Aryclenio Barros",
-      "phone": "(84)99982-8379",
-      "birth": "21/10/1997",
-      "cnh": "003434324",
-      "cnhType": "A",
-      "cpf": "106.048.764-61"
-    },
-    {
-      "name": "Aryclenio Barros",
-      "phone": "(84)99982-8379",
-      "birth": "21/10/1997",
-      "cnh": "003434324",
-      "cnhType": "A",
-      "cpf": "106.048.764-61"
-    }
-  ]);
+  const [drivers, setDrivers] = useState([]);
+
+  useEffect(() => {
+    fetchDrivers();
+  }, [])
+  const fetchDrivers = () => {
+    api.get("drivers").then((response) => {
+      setDrivers(response.data);
+    });
+  };
+
 
   return (
     <AppContext.Provider value={[drivers, setDrivers]}>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { AppContext } from '../../AppContext'
+import api from '../../services/api'
 
 export default function Home() {
   const [drivers, setDrivers] = useContext(AppContext)
 
-  console.log(drivers)
+  console.log(drivers);
   return (
     <div>
       <h1>Home</h1>
